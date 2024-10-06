f7b84c81f0ba6896a587dc3bb2398bc2ee2a01a9
karem <karem@KAREEM_ALI>
2024-10-06 21:07:08 +0300

    Add project files.

---
+++ GameZone/Services/CategoriesService.cs
@@ -0,0 +1,20 @@
+﻿namespace GameZone.Services
+{
+    public class CategoriesService : ICategoriesService
+    {
+        private readonly AppDbContext _context;
+
+        public CategoriesService(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public IEnumerable<SelectListItem> GetSelectList()
+        {
+            return _context.Categorys
+                            .Select(c => new SelectListItem() { Value = c.Id.ToString(), Text = c.Name })
+                            .OrderBy(c => c.Text)
+                            .AsNoTracking().ToList();
+        }
+    }
+}
---
+++ GameZone/Services/IGamesService.cs
@@ -0,0 +1,11 @@
+﻿namespace GameZone.Services
+{
+    public interface IGamesService
+    {
+        IEnumerable<Game> GetAll();
+        Game? GetById(int id);
+        Task create(CreateGameFormViewModel model);
+        Task<Game?> Edit(EditGameFormViewModel model);
+        bool Delete(int id);
+    }
+}
